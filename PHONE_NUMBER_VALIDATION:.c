dada06241ba4da5c70031618230d14669b39d4aa
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-10-11 15:55:16 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     string s;     cin>>s;     if (s[0]!=0 && s.length()==10)     {         cout<<"Valid";     }     else{         cout<<"Invalid";     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if (s[0]!=0 && s.length()==10)
+    {
+        cout<<"Valid";
+    }
+    else{
+        cout<<"Invalid";
+    }
+}
+
