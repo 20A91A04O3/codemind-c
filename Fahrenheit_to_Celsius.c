2fdee5a83cb777b94f38da04c9e2f0f086851783
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-10-11 08:25:57 +0530

    #include<bits/stdc++.h> #include<iostream> using namespace std; int main(){     float f;     float ans;     cin>>f;     ans=((f-32)*5)/9;     cout<<fixed<<setprecision(2)<<ans; }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    float f;
+    float ans;
+    cin>>f;
+    ans=((f-32)*5)/9;
+    cout<<fixed<<setprecision(2)<<ans;
+}
