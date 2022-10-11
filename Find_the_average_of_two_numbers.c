da0c3f338e9297cc80f2149ac1dc4e97a4c195f3
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-10-11 11:11:49 +0530

    #include<iostream> #include<iomanip> using namespace std; int main(){     float a,b;     float c;     cin>>a>>b;     c=(a+b)/2;     cout<<fixed<<setprecision(4)<<c;
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip> 
+using namespace std;
+int main(){
+    float a,b;
+    float c;
+    cin>>a>>b;
+    c=(a+b)/2;
+    cout<<fixed<<setprecision(4)<<c;
+    
+}
